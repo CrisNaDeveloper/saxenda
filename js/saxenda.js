3cb416781470d8c89c70e419ab8eae9b846d06af
Cristina <c_navarro_martinez@hotmail.com>
2020-11-26 16:42:49 +0100

    con autenticación

--- js/saxenda.js
+++ js/saxenda.js
@@ -1,22 +1,3 @@
-function parametroURL(_par) {
-
-    var _p = null;
-    if (location.search) location.search.substr(1).split("&").forEach(function(pllv) {
-        var s = pllv.split("="), //separamos llave/valor
-            ll = s[0],
-            v = s[1] && decodeURIComponent(s[1]); //valor hacemos encode para prevenir url encode
-        if (ll == _par) { //solo nos interesa si es el nombre del parametro a buscar
-            if (_p == null) {
-                _p = v; //si es nula, quiere decir que no tiene valor, solo textual
-            } else if (Array.isArray(_p)) {
-                _p.push(v); //si ya es arreglo, agregamos este valor
-            } else {
-                _p = [_p, v]; //si no es arreglo, lo convertimos y agregamos este valor
-            }
-        }
-    });
-    return _p;
-}
 var config = {
     apiKey: "AIzaSyC7k-ZwvRr4r0SW6x-LzAhGT7XnssZy09A",
     authDomain: "saxenda-6f20a.firebaseapp.com",
@@ -100,21 +81,15 @@ function authenticate() {
 
 
 
+
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        var name = user.displayName;
-
-        /* If the provider gives a display name, use the name for the
-        personal welcome message. Otherwise, use the user's email. */
-        var welcomeName = name ? name : user.email;
-
-        userIdToken = user.getIdToken();
-
-
+        user.getIdToken().then(function(data) {
+            console.log(data)
+        });
     }
 });
-
-
 
 
 
