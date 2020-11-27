8a41b0f3644af8675da8e160855da611939bb572
Cristina <c_navarro_martinez@hotmail.com>
2020-11-27 22:53:43 +0100

    con autenticación

--- js/saxenda.js
+++ js/saxenda.js
@@ -37,8 +37,8 @@ function otramaneralogin() {
         signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+           // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+           // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
             //     firebase.auth.GithubAuthProvider.PROVIDER_ID,
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
             //     firebase.auth.PhoneAuthProvider.PROVIDER_ID,
