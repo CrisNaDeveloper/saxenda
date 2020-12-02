90f73f23f31c1ca2aed4e934cf5ec9825fbf0026
Cristina <c_navarro_martinez@hotmail.com>
2020-12-02 15:22:39 +0100

    mas campos

--- js/saxenda.js
+++ js/saxenda.js
@@ -302,7 +302,7 @@ function cargaresultado(email) {
 
                 res = doc.data();
 
-                $("#tbodyresultado").append("<tr id='trdentro'><td>" + res.fecha.toLocaleDateString("es-ES", options) + "</td><td>" + res.cantidad + "</td><td>" + res.peso + "</td><td>" + res.glucosa + "</td><td>" + res.presion + "</td><td>" + res.hba + "</td><td>" + res.otros + "</td></tr>");
+                $("#tbodyresultado").append("<tr id='trdentro'><td>" + res.fecha + "</td><td>" + res.cantidad + "</td><td>" + res.peso + "</td><td>" + res.glucosa + "</td><td>" + res.presion + "</td><td>" + res.hba + "</td><td>" + res.otros + "</td></tr>");
 
                 $("#tbodyresultado").trigger("create");
                 $("#tablaresultados").table("refresh");
