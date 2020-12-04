09cc8b78939342d65583c66cd289d0dc33706a77
Cristina <c_navarro_martinez@hotmail.com>
2020-12-04 12:59:23 +0100

    cambio prioridad

--- js/saxenda.js
+++ js/saxenda.js
@@ -307,7 +307,7 @@ function cargaresultado(email) {
 
                 res = doc.data();
                 var fechfor = new Date(res.fecha);
-                $("#tbodyresultado").append("<tr id='trdentro'><td>" + fechfor.toLocaleDateString("es-ES", options) + "</td><td>" + res.cantidad + "</td><td>" + res.peso + "</td><td>" + res.glucosa + "</td><td>" + res.presion + "</td><td>" + res.hba + "</td><td>" + res.otros + "</td><td><a href='#' onclick=borrar('" + email + "') data-role='button' data-transition='flip' data-icon='trash'></a></td></tr>");
+                $("#tbodyresultado").append("<tr id='trdentro'><td>" + fechfor.toLocaleDateString("es-ES", options) + "</td><td>" + res.cantidad + "</td><td>" + res.peso + "</td><td>" + res.glucosa + "</td><td>" + res.presion + "</td><td>" + res.hba + "</td><td>" + res.otros + "</td><td><a href='#' onclick=borrar('" + email + "') data-role='button' data-transition='flip' data-icon='trash-o'></a></td></tr>");
 
                 $("#tbodyresultado").trigger("create");
                 $("#tablaresultados").table("refresh");
